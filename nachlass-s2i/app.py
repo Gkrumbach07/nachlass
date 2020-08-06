dc16d31e62cc5a1732a70572919e83801bbcaf0d
Gage Krumbach <gkrumbach@gmail.com>
2020-08-06 11:38:48 -0500

    Update app.py

--- nachlass-s2i/app.py
+++ nachlass-s2i/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, request, url_for
-from prometheus_client import make_wsgi_app, Summary, Counter
+from flask_cors import CORS
+from prometheus_client import make_wsgi_app, Summary, Counter, Histogram
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 import base64
@@ -10,15 +11,14 @@ import cloudpickle
 import sys
 import os
 import pandas as pd
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-METRICS_PREFIX = os.getenv("S2I_APP_METRICS_PREFIX", "pipeline")
+METRICS_PREFIX = os.getenv("S2I_APP_METRICS_PREFIX", "nachlass")
 
 PREDICTION_TIME = Summary('%s_processing_seconds' % METRICS_PREFIX, 'Time spent processing predictions')
-PREDICTIONS = Counter('%s_predictions_total' % METRICS_PREFIX, 'Total predictions for a given label', ['value'])
+
 app.model = None
 
 @app.route('/')
@@ -34,26 +34,55 @@ def predict():
       if len(args.columns) == 1 and len(args.values) > 1:
           # convert to series
           args = args.squeeze()
-
+      else:
+          args = [args.squeeze()]
     else:
       args = cPloads(base64.b64decode(request.form['args']))
     try:
         predictions = app.model.predict(args)
         for v in predictions:
-            PREDICTIONS.labels(v).inc()
+            app.observe_prediction(v)
         return json.dumps(predictions.tolist())
     except ValueError as ve:
         return str(ve)
     except Exception as e:
         return str(e)
 
+def classifier_prediction_recorder(p):
+    def record(v):
+        p.labels(v).inc()
+    return record
+
+def regressor_prediction_recorder(p):
+    def record(v):
+        p.observe(v)
+    return record
+
+
 try:
     import json
     from sklearn.pipeline import Pipeline
-    app.model = Pipeline([(k, cPload(open(v, "rb"))) for k, v in json.load(open("stages.json", "r"))])
-
+    stages = json.load(open("stages.json", "r"))
+    print("stages are: %r" % stages)
+    app.model = Pipeline([(k, cPload(open(v, "rb"))) for k, v in stages])
+    if app.model.steps[-1][1]._estimator_type == 'classifier':
+        pm = Counter('%s_predictions_total' % METRICS_PREFIX, 'Total predictions for a given label', ['value'])
+        app.observe_prediction = classifier_prediction_recorder(pm)
+    elif app.model.steps[-1][1]._estimator_type == 'regressor':
+        pm = Histogram("%s_predictions" % METRICS_PREFIX, "Prediction values for this pipeline")
+        app.observe_prediction = regressor_prediction_recorder(pm)
+        
+      
 except Exception as e:
+    import time
+    import os
+    
     print(str(e))
+    
+    if os.environ.get("NACHLASS_DEBUG") is not None:
+        print("Sleeping for ten minutes")
+        time.sleep(600)
+    
     sys.exit()
 
 app_dispatch = DispatcherMiddleware(app, {
